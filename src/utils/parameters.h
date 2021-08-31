a1a54c3dcd269b4d38bdf87cf504fc4acd39b101
pedromtelho <pedromtelho@hotmail.com>
2021-08-31 08:25:57 -0300

    new parameters

--- src/utils/parameters.h
+++ src/utils/parameters.h
@@ -7,9 +7,9 @@
 const float pi = 3.1416;
 const float g = 9.81;       // m/s^2
 
-const float a2 = pow(1.16, -7.0);
-const float a1 = pow(4.488, -12.0);
-
+const float a2 = pow(1.16, -7.0); // (Ra * kd)/(Km * es)
+const float a1 = pow(4.488, -12.0); // (Ra * b + Km^2)/(Km * es)
+const float kt = pow(1.726, -8.0); // constante de sustentação
 // Quadcopter dimensions
 const float m = 30.0e-3;    // kg
 const float I_xx = 16.0e-6; // kg.m^2
